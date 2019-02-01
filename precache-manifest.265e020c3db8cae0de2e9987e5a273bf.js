8746d2ff6dffdaa1cf93f6e131fd9c49eda054a8
David Andrews <contactdardino@gmail.com>
2019-02-01 17:03:07 +0000

    Updates

---
+++ precache-manifest.265e020c3db8cae0de2e9987e5a273bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "83f1f207c9ae7b262776",
+    "url": "/simple-gbp-euro-converter/static/js/runtime~main.83f1f207.js"
+  },
+  {
+    "revision": "a8337072c278d88daf1f",
+    "url": "/simple-gbp-euro-converter/static/js/main.a8337072.chunk.js"
+  },
+  {
+    "revision": "7c3aec79f3ae62290828",
+    "url": "/simple-gbp-euro-converter/static/js/1.7c3aec79.chunk.js"
+  },
+  {
+    "revision": "a8337072c278d88daf1f",
+    "url": "/simple-gbp-euro-converter/static/css/main.230736c3.chunk.css"
+  },
+  {
+    "revision": "d5bcb5ff9473e4188c0ac82f43283adb",
+    "url": "/simple-gbp-euro-converter/index.html"
+  }
+];
